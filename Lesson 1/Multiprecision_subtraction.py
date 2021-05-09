a7146579919c88d3c8a0e48e6b31e58e532a6f0f
LilThawg <72289126+phamthanh29@users.noreply.github.com>
2021-05-09 11:52:25 +0700

    Update Multiprecision_subtraction.py

--- Lesson 1/Multiprecision_subtraction.py
+++ Lesson 1/Multiprecision_subtraction.py
@@ -1,30 +1,21 @@
-#Algorithm 02
-def Multiprecision_subtraction(a,b) :
-    e = 0
-    c = [''] * len(a)
-    for i in range(len(a),0,-1):
-        c[i-1] = (a[i-1]-b[i-1]-e) %  2**8
-        if a[i-1]-b[i-1]-e  < 0 :
-            e = 1
-        else : e = 0
-    return (e,c)
+# Alogorithm 01
+import math
 
-def check_sub (a,b,c) :
-    t = len(a)
-    A = 0
-    B = 0
-    C = 0
+def Multiprecision_addition(a, b, w=8, p=2147483647):
+    m = math.ceil(math.log(p, 2))
+    t = math.ceil(m / w)
+    c = [''] * t
+    e = 0
     for i in range(t):
-        A += a[i] * pow(2, (t - 1 - i) * 8)
-        B += b[i] * pow(2, (t - 1 - i) * 8)
-        C += c[i] * pow(2, (t - 1 - i) * 8)
-    if (A-B)%(2**(8*t)) == C :
-        return True
-    else : return False
+        c[t-1-i] = (a[t-1-i] - b[t-1-i] - e) % (2**w)
+        if a[t-1-i] - b[t-1-i] - e < 0 :
+            e = 1
+        else :
+            e = 0
+    return e,c
 
 if __name__ == '__main__':
-    a = [0,11,173,248]
-    b = [0,1,226,64]
-    print(Multiprecision_subtraction(a,b))
-    c = Multiprecision_subtraction(a,b)[1]
-    print(check_sub(a,b,c))
+    a = [57,169,36,27]
+    b = [0,98,34,62]
+    print(Multiprecision_addition(a,b))
+
