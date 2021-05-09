0254f1b3562ca2e8f6131cbe31cacc209f1682f1
LilThawg <72289126+phamthanh29@users.noreply.github.com>
2021-05-09 15:35:03 +0700

    Create Subtraction_in_Fp.py

---
+++ Lesson 2/Subtraction_in_Fp.py
@@ -0,0 +1,53 @@
+# Alogorithm 03
+import math
+
+def Multiprecision_addition(a, b, w=8, p=2147483647):
+    m = math.ceil(math.log(p, 2))
+    t = math.ceil(m / w)
+    c = [''] * t
+    e = 0
+    for i in range(t):
+        c[t-1-i] = (a[t-1-i] + b[t-1-i] + e) % (2**w)
+        if a[t-1-i] + b[t-1-i] + e >= 2**w :
+            e = 1
+        else :
+            e = 0
+    return e,c
+def Multiprecision_subtraction(a, b, w=8, p=2147483647):
+    m = math.ceil(math.log(p, 2))
+    t = math.ceil(m / w)
+    c = [''] * t
+    e = 0
+    for i in range(t):
+        c[t-1-i] = (a[t-1-i] - b[t-1-i] - e) % (2**w)
+        if a[t-1-i] - b[t-1-i] - e < 0 :
+            e = 1
+        else :
+            e = 0
+    return e,c
+def Subtraction_in_Fp (a,b,p):
+    e,c = Multiprecision_subtraction(a,b)
+    if e == 0 :
+        return e,c
+    else :
+        e,c = Multiprecision_addition(c,p)
+        return e,c
+def number_to_array (a,w=8,p=2147483647):
+    m = math.ceil(math.log(p, 2))
+    t = math.ceil(m / w)
+    arr = ['']*t
+    tmp = a
+    sum = 0
+    for i in range(t):
+        x = pow(2, ((t - 1 - i) * w))
+        arr[i] = tmp // x
+        sum += arr[i] * x
+        tmp = a - sum
+    return arr
+if __name__ == '__main__':
+       a = [0,11,173,248]
+       b = [0,1,226,64]
+       p = number_to_array(2147483647)
+       print(Subtraction_in_Fp(a,b,p))
+
+
