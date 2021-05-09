6ffefa0025dc9031efd48f26ee582872c72e55d1
LilThawg <72289126+phamthanh29@users.noreply.github.com>
2021-05-09 11:22:28 +0700

    Update and rename Array.py to number_to_array.py

---
+++ Lesson 1/number_to_array.py
@@ -0,0 +1,16 @@
+import math
+def number_to_array (a,w=8,p=2147483647):
+    m = math.ceil(math.log(p, 2))
+    t = math.ceil(m / w)
+    arr = ['']*t
+    tmp = a
+    sum = 0
+    for i in range(t):
+        x = pow(2, ((t - 1 - i) * w))
+        arr[i] = tmp // x
+        sum += arr[i] * x
+        tmp = a - sum
+    return arr
+
+if __name__ == '__main__':
+    print(number_to_array(2147483647))
