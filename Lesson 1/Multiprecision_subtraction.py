265f4c9e5bc4bdd895e18de09a789ae2ae7d82cb
LilThawg <72289126+phamthanh29@users.noreply.github.com>
2021-05-09 14:16:55 +0700

    Update Multiprecision_subtraction.py

--- Lesson 1/Multiprecision_subtraction.py
+++ Lesson 1/Multiprecision_subtraction.py
@@ -1,7 +1,7 @@
 # Alogorithm 02
 import math
 
-def Multiprecision_addition(a, b, w=8, p=2147483647):
+def Multiprecision_subtraction(a, b, w=8, p=2147483647):
     m = math.ceil(math.log(p, 2))
     t = math.ceil(m / w)
     c = [''] * t
@@ -17,5 +17,5 @@ def Multiprecision_addition(a, b, w=8, p=2147483647):
 if __name__ == '__main__':
     a = [57,169,36,27]
     b = [0,98,34,62]
-    print(Multiprecision_addition(a,b))
+    print(Multiprecision_subtraction(a,b))
 
