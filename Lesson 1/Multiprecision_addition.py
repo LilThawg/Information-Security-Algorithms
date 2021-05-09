81fb71bf83f0d56841c479b7bcd6721cc4968975
LilThawg <72289126+phamthanh29@users.noreply.github.com>
2021-05-09 11:49:25 +0700

    Update Multiprecision_addition.py

--- Lesson 1/Multiprecision_addition.py
+++ Lesson 1/Multiprecision_addition.py
@@ -1,30 +1,21 @@
-#Alogorithm 01
-def Multiprecision_addition(a,b) :
+# Alogorithm 01
+import math
+
+def Multiprecision_addition(a, b, w=8, p=2147483647):
+    m = math.ceil(math.log(p, 2))
+    t = math.ceil(m / w)
+    c = [''] * t
     e = 0
-    c = ['']*len(a)
-    for i in range(len(a),0,-1):
-        c[i-1] = (a[i-1]+b[i-1]+e) %  2**8
-        if a[i-1]+b[i-1]+e  >= 2**8 :
-            e = 1
-        else : e = 0
-    return (e,c)
-  
-def check_add (a,b,c) :
-    t = len(a)
-    A = 0
-    B = 0
-    C = 0
     for i in range(t):
-        A += a[i] * pow(2, (t - 1 - i) * 8)
-        B += b[i] * pow(2, (t - 1 - i) * 8)
-        C += c[i] * pow(2, (t - 1 - i) * 8)
-    if (A+B)%(2**(8*t)) == C :
-        return True
-    else : return False
-    
+        c[t-1-i] = (a[t-1-i] + b[t-1-i] + e) % (2**w)
+        if a[t-1-i] + b[t-1-i] + e >= 2**w :
+            e = 1
+        else :
+            e = 0
+    return e,c
+
 if __name__ == '__main__':
-    a = [57,169,36,27]
-    b = [0,98,34,62]
-    print(Multiprecision_addition(a, b))
-    c = Multiprecision_addition(a,b)[1]
-    print(check_add(a,b,c))
+    a = [157,0,173,23]
+    b = [169,1,0,64]
+    print(Multiprecision_addition(a,b))
+
