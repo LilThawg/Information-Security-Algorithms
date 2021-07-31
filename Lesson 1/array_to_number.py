7f110de382b4051b17579e856ddeaee117ee5935
LilThawg <72289126+phamthanh29@users.noreply.github.com>
2021-07-31 21:53:37 +0700

    Update array_to_number.py

--- Lesson 1/array_to_number.py
+++ Lesson 1/array_to_number.py
@@ -1,5 +1,5 @@
 import math
-#biểu diễn ma trận A thành số
+#biểu diễn arr A thành số
 def array_to_number(arr,w=8,p=2147483647):
     m = math.ceil(math.log(p, 2)) #m = làm tròn lên log cơ số 2 của p
     t = math.ceil(m / w) # với w bits, w là bội số của 8, t được tính = làm tròn lên (m / w)
