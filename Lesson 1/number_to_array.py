229f8641360d7a00b7c1280f1215ad928267d110
LilThawg <72289126+phamthanh29@users.noreply.github.com>
2021-07-31 21:38:05 +0700

    Update number_to_array.py

--- Lesson 1/number_to_array.py
+++ Lesson 1/number_to_array.py
@@ -1,16 +1,14 @@
 import math
-
+#biểu diễn số A thành một ma trận
 def number_to_array (a,w=8,p=2147483647):
-    m = math.ceil(math.log(p, 2))
-    t = math.ceil(m / w)
-    arr = ['']*t
-    tmp = a
-    sum = 0
+    m = math.ceil(math.log(p, 2))  #m = làm tròn lên log cơ số 2 của p
+    t = math.ceil(m / w) # với w bits, w là bội số của 8, t được tính = làm tròn lên (m / w)
+    arr = ['']*t # khởi tạo arr gồm t phần tử
+    tmp = a #khởi tạo tmp = a đóng vai trò như là phần dư sau mỗi lần tính toán
     for i in range(t):
         x = pow(2, ((t - 1 - i) * w))
         arr[i] = tmp // x
-        sum += arr[i] * x
-        tmp = a - sum
+        tmp = tmp % (arr[i]*x)
     return arr
 
 if __name__ == '__main__':
