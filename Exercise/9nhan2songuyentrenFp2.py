a0738bd3428d4e8b0e9d02c8e61a501b7b62e77b
LilThawg <72289126+phamthanh29@users.noreply.github.com>
2021-08-02 23:04:16 +0700

    Create 9nhan2songuyentrenFp2.py

---
+++ Exercise/9nhan2songuyentrenFp2.py
@@ -0,0 +1,62 @@
+# Nhân hai số nguyên lớn trên trường hữu hạn 2
+import math
+def convert_uv_to_u_v(uv):
+    bin_uv = str(bin(uv)[2:])
+    if len(bin_uv) < 16 :
+        bin_uv = "0"*(16-len(bin_uv))+bin_uv
+    u = int(bin_uv[:8],2)
+    v = int(bin_uv[8:],2)
+    return u,v
+def Integer_multiprecision (a, b, w=8, p=2147483647):
+    a = a[::-1]
+    b = b[::-1]
+    m = math.ceil(math.log(p, 2))
+    t = math.ceil(m / w)
+    c = [0]*2*t
+    for i in range(t):
+        u = 0
+        for j in range(t):
+            uv = c[i+j]+a[i]*b[j]+u
+            u,v = convert_uv_to_u_v(uv)
+            c[i+j] = v
+        c[i+t] = u
+    return c[::-1]
+
+def number_to_array (a,w=8,p=2147483647):
+    m = math.ceil(math.log(p, 2))
+    t = math.ceil(m / w)
+    arr = ['']*t
+    tmp = a
+    sum = 0
+    for i in range(t):
+        x = pow(2, ((t - 1 - i) * w))
+        arr[i] = tmp // x
+        sum += arr[i] * x
+        tmp = a - sum
+    return arr
+
+def array_to_number(arr,w=8,p=2147483647):
+    m = math.ceil(math.log(p, 2))
+    t = math.ceil(m / w)
+    a = 0
+    for i in range(t):
+        x = pow(2, ((t - 1 - i) * w))
+        a += arr[i] * x
+    return a
+
+if __name__ == '__main__':
+       p, W = map(int, input().split())
+       a = list(map(int, input().strip().split()))
+       b = list(map(int, input().strip().split()))
+       res = str(Integer_multiprecision(a,b,W,p))
+       print("c=a.b="+res.replace(', ','   '))
+
+'''
+Input:
+2147483647 8 
+0   8   1   103
+0   0   127   37
+
+Output:
+c=a.b=[0   0   0   3   249   218   76   227]
+'''
