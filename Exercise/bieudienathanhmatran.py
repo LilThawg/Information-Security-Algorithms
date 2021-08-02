bc5fcbf330f91ffc6e35f61f5e8cfaee7d797356
LilThawg <72289126+phamthanh29@users.noreply.github.com>
2021-08-02 22:33:42 +0700

    Create bieudienathanhmatran.py

---
+++ Exercise/bieudienathanhmatran.py
@@ -0,0 +1,31 @@
+import math
+#Biểu diễn số A thành một ma trận
+def number_to_array(a, w, p):
+    m = math.ceil(math.log(p, 2))
+    t = math.ceil(m / w)
+    arr = []
+    tmp = a
+    for i in range(t):
+        x = pow(2, ((t - 1 - i) * w))
+        arr.append(tmp // x)
+        tmp = tmp % x
+    return arr
+
+
+if __name__ == '__main__':
+    p, W, a = map(int, input().split())
+    # p = 2147483647
+    # W = 8
+    # a = 38762497
+    res = str(number_to_array(a, W, p))
+    print("A= " + res.replace(', ', '   '))
+
+'''
+Input:
+2147483647 8 38762497
+
+
+
+Output:
+A= [2    79   120     1]
+'''
